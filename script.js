222ee1eb4b6fdb73945b3de3f72bd89086ee9802
matdms <mathieu.desmoulins@gmail.com>
2022-01-21 23:59:17 +0100

    generate gameboard table

--- script.js
+++ script.js
@@ -8,3 +8,20 @@ const Gameboard = () => {
 
 const gameboard = Gameboard()
 console.table(gameboard.array)
+
+// render gameboard
+const gameDiv = document.getElementById('game')
+const table = document.createElement('table')
+table.className = 'gameboardTable'
+
+for (let i = 0; i<3; i++) {
+  const tr = document.createElement('tr')
+  for (let j = 0; j<3; j++) {
+    const td = document.createElement('td')
+    td.className = 'gameboardCell'
+    td.textContent = `Case ${i}x${j}`
+    tr.appendChild(td)
+  }
+  table.appendChild(tr)
+}
+gameDiv.appendChild(table)
